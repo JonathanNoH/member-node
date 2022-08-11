8d23dceba0248e4edcb4df95a48918761ab5e677
JonathanNoH <jonathan897h@gmail.com>
2022-08-11 12:17:30 -0600

    add author to message schema

--- models/message.js
+++ models/message.js
@@ -5,7 +5,8 @@ const MessageSchema = new Schema (
   {
     title: {type: String, required: true, minLength: 1, maxLength: 100},
     content: {type: String, required: true, minLength: 1, maxLength: 1000},
-    timestamp: {type: Date, required: true}
+    timestamp: {type: Date, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'Member', required: true},
   }
 );
 
